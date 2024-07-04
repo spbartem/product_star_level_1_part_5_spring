02c2dfdae773beecfd903859bbf0386ac5def82c
spbartem <spbartemm@gmail.com>
2024-07-04 23:23:43 +0300

    lesson 5 servlet - workshop 2 completed

---
+++ lesson_5_servlets/workshop-servlets/src/main/java/ru/productstar/servlets/report/ComplexReportServlet.java
@@ -0,0 +1,38 @@
+package ru.productstar.servlets.report;
+
+import jakarta.servlet.AsyncContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(value = "/complex-report", asyncSupported = true)
+public class ComplexReportServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        long start = System.currentTimeMillis();
+
+        /*try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }*/
+
+        final AsyncContext asyncContext = req.startAsync();
+        new Thread(() -> {
+            try {
+                Thread.sleep(3000);
+                asyncContext.getResponse().getWriter().println("Success");
+                asyncContext.complete();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }).start();
+
+        req.getServletContext().log(String.format("[ComplexReportServlet] Thread %s, %d", Thread.currentThread().getName(), System.currentTimeMillis() - start));
+//        resp.getWriter().println("Success");
+    }
+}
